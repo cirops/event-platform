bf6ce60db8bb7516731f0a87b4d12aaf07f7c500
cirops <cirops@gmail.com>
2022-06-23 19:42:24 -0300

    feat: adds main visual elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { gql, useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { client } from "./lib/apollo";
 
+import { Event } from "./pages/Event";
+
 const GET_LESSONS_QUERY = gql`
   query {
     lessons {
@@ -11,20 +13,8 @@ const GET_LESSONS_QUERY = gql`
   }
 `;
 
-interface Lesson {
-  id: string;
-  title: string;
-}
-
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
+  return <Event />;
 }
 
 export default App;
---
+++ src/components/Lesson.tsx
@@ -0,0 +1,40 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export const Lesson = (props: LessonProps) => {
+  const isLessonAvailable = false;
+
+  return (
+    <a href="#">
+      <span className="">{props.availableAt.toString()}</span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+          <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300">
+            {props.type == 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
+        </header>
+        <strong className="text-gray-200 mt-5 block">
+          {props.title}
+        </strong>
+      </div>
+    </a>
+  );
+};
